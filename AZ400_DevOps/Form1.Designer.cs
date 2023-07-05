9aa7055172e8d686ad3fd7a5a864701f4abc9d06
Ian Formanes <crof09@yahoo.com>
2023-07-05 15:33:06 +0800

    Add project files.

---
+++ AZ400_DevOps/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace AZ400_DevOps
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            btnClose = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(119, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(526, 65);
+            label1.TabIndex = 0;
+            label1.Text = "AZ-400 Azure DevOps";
+            // 
+            // btnClose
+            // 
+            btnClose.Location = new Point(245, 191);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(255, 45);
+            btnClose.TabIndex = 1;
+            btnClose.Text = "&Close";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnClose);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnClose;
+    }
+}
---
+++ AZ400_DevOps/Form1.cs
@@ -0,0 +1,15 @@
+namespace AZ400_DevOps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
